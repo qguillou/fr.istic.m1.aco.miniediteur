fa35b28d878ec52791776b751957369b41996afc
Quentin Guillou <guillou.quentin.29@gmail.com>
2015-10-11 18:17:51 +0200

    Adding of javadoc

--- fr.istic.m1.aco.miniediteur.v1/src/receiver/ClipBoard.java
+++ fr.istic.m1.aco.miniediteur.v1/src/receiver/ClipBoard.java
@@ -1,20 +1,36 @@
 package receiver;
 
 /**
- * @(#) ClipBoard.java
+ * ClipBoard
+ * The clipboard
+ * @author Yann Jegu & Quentin Guillou
+ * @version 1
  */
-
 public class ClipBoard {
+	//the clipboard contains
 	private String text;
-		
+	
+	/**
+	 * ClipBoard() - Constructor
+	 * initialize the variables
+	 */
 	public ClipBoard(){
 		this.text = "";
 	}
-		
+	
+	/**
+	 * getText()
+	 * @return text: the contain of clipboard
+	 */
 	public String getText() {
 		return text;
 	}
 	
+	/**
+	 * setText()
+	 * update the content of clipboard
+	 * @param text: new content of clipboard
+	 */
 	public void setText(String text) {
 		this.text = text;
 	}	
