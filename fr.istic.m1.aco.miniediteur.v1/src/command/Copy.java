699a2c42b91edcfc14560de0c2d1e5bf57167c79
JEGU Yann <y.jegu@hotmail.fr>
2015-10-16 17:11:54 +0200

    Adding DP Command + Invoker

---
+++ fr.istic.m1.aco.miniediteur.v1/src/command/Copy.java
@@ -0,0 +1,25 @@
+package command;
+
+import receiver.EditorEngine;
+
+public class Copy implements Command {
+
+	private EditorEngine engine;
+	
+	/**
+	 * Construct the Command Copy
+	 * @param engine the engine which will receive the command
+	 */
+	public Copy(EditorEngine engine) {
+		this.engine = engine;
+	}
+	
+	/**
+	 * Execute the Copy Command
+	 */
+	@Override
+	public void execute() {
+		engine.copy();
+	}
+
+}
---
+++ fr.istic.m1.aco.miniediteur.v1/src/command/Type.java
@@ -0,0 +1,25 @@
+package command;
+
+import receiver.EditorEngine;
+
+public class Type implements Command {
+
+	private EditorEngine engine;
+	
+	/**
+	 * Construct the Command Type
+	 * @param engine the engine which will receive the command
+	 */
+	public Type(EditorEngine engine) {
+		this.engine = engine;
+	}
+	
+	/**
+	 * Execute the Type Command
+	 */
+	@Override
+	public void execute() {
+		engine.type();
+	}
+
+}
