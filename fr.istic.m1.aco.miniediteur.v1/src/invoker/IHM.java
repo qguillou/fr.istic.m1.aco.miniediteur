0f44255ff13a4ac5561064ad1b620cdc89099fc4
Yann JEGU <y.jegu@hotmail.fr>
2015-10-17 20:32:00 +0200

    Updating IHM + gitignore

--- fr.istic.m1.aco.miniediteur.v1/src/invoker/IHM.java
+++ fr.istic.m1.aco.miniediteur.v1/src/invoker/IHM.java
@@ -1,9 +1,12 @@
 package invoker;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -13,59 +16,67 @@ public class IHM extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	
-	private JTextArea area = new JTextArea(30,120);
+	private JTextArea area;
 
 	public IHM() {
 		super();
         
+		// Define dimension of text area of the editor
+		area = new JTextArea(30, 120);
+		
+		// Define font size and style
         area.setFont(new Font("Monospaced",Font.PLAIN,12));
+        
+        // Pane which allows scrolling
 		JScrollPane scroll = new JScrollPane(area,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		add(scroll, BorderLayout.CENTER);
 		
+		// Adding the menu bar to the editor
 		JMenuBar JMB = new JMenuBar();
 		setJMenuBar(JMB);
-//		JMenu file = new JMenu("File");
-//		JMenu edit = new JMenu("Edit");
-//		JMB.add(file); JMB.add(edit);
 		
-//		file.add("New");
-//		file.add("Open");
-//		file.add("Save");
-//		file.add("Quit");
-//		file.add("Save As");
-//		file.addSeparator();
-//		
-//		for(int i=0; i<4; i++)
-//			file.getItem(i).setIcon(null);
-//		
-//		edit.add("Cut");
-//		edit.add("Copy");
-//		edit.add("Paste");
-//
-//		edit.getItem(0).setText("Cut");
-//		edit.getItem(1).setText("Copy");
-//		edit.getItem(2).setText("Paste");
+		// Adding item menu Edit to the editor
+		JMenu edit = new JMenu("Edit");
+		JMB.add(edit);
 		
+		// Adding items to the menu Edit
+		edit.add("Undo");
+		edit.add("Redo");
+		edit.addSeparator();
+		edit.add("Cut");
+		edit.add("Copy");
+		edit.add("Paste");
+//		edit.getItem(0).setText("Undo");
+//		edit.getItem(1).setText("Redo");
+//		edit.getItem(3).setText("Cut");
+//		edit.getItem(4).setText("Copy");
+//		edit.getItem(5).setText("Paste");
+		
+		// Adding tool bar below the menu bar
 		JToolBar tool = new JToolBar();
 		add(tool,BorderLayout.NORTH);
-//		tool.add("New");
-//		tool.add("Open");
-//		tool.add("Save");
-//		tool.addSeparator();
-//		
-//		JButton cut = tool.add("Cut"), cop = tool.add("Copy"), pas = tool.add("Paste");
 		
-//		cut.setText(null);
-//		cut.setIcon(new ImageIcon("cut.gif"));
-//		cop.setText(null);
-//		cop.setIcon(new ImageIcon("copy.gif"));
-//		pas.setText(null);
-//		pas.setIcon(new ImageIcon("paste.gif"));
+		// Create buttons for the tool bar
+		JButton cut = new JButton("Cut");
+		JButton copy = new JButton("Copy");
+		JButton paste = new JButton("Paste");
+		JButton erase = new JButton("Erase");
+		JButton undo = new JButton("Undo");
+		JButton redo = new JButton("Redo");
 		
-//		Save.setEnabled(false);
-//		SaveAs.setEnabled(false);
+		// Adding buttons to the tool bar
+		tool.add(cut);
+		tool.add(copy);
+		tool.add(paste);
+		tool.add(erase);
+		tool.addSeparator(new Dimension(40, 0));
+		tool.add(undo);
+		tool.add(redo);
 		
+		// The editor will be ended by clicking on the cross
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		// Update new preferred size of the Components 
 		pack();
 		setVisible(true);
 	}
