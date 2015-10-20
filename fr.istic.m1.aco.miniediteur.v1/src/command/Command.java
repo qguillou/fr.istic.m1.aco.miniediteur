132fa310f96943ea3aeb79eb3d60142b4523bbe5
Quentin Guillou <guillou.quentin.29@gmail.com>
2015-10-20 17:47:30 +0200

    Update Command interface

--- fr.istic.m1.aco.miniediteur.v1/src/command/Command.java
+++ fr.istic.m1.aco.miniediteur.v1/src/command/Command.java
@@ -1,13 +1,6 @@
 package command;
 
 public interface Command {
+	
 	public void execute();
-
-	public String getText();
-	
-	public void setText(String text);
-	
-	public int getSelectionStart();
-	
-	public int getSelectionEnd();
 }
