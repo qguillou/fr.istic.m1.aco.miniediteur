ca1e88362626ad887d46f0ac19dfc74d4ad63fe9
Quentin Guillou <guillou.quentin.29@gmail.com>
2015-10-21 13:53:44 +0200

    Add javadoc of Observer

--- fr.istic.m1.aco.miniediteur.v1/src/observer/Subject.java
+++ fr.istic.m1.aco.miniediteur.v1/src/observer/Subject.java
@@ -2,13 +2,33 @@ package observer;
 
 import java.util.Collection;
 
+/**
+ * Subject <br/>
+ * @author Yann Jegu & Quentin Guillou
+ * @version 1.0
+ */
 public abstract class Subject {
 	
+	//Collection of observers (TextArea)
 	protected Collection<Observer> observers;
 	
+	/**
+	 * notifyObservers() <br/>
+	 * Notify the observers of a content change
+	 */
 	public abstract void notifyObservers();
 	
+	/**
+	 * registerObserver() <br/>
+	 * add a observer in Collection
+	 * @param o: new observer
+	 */
 	public abstract void registerObserver(Observer o);
 	
+	/**
+	 * unregisterObserver() <br/>
+	 * delete a observer in Collection
+	 * @param o: new Observer
+	 */
 	public abstract void unregisterObserver(Observer o);
 }
