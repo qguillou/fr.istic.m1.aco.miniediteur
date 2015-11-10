245ccca52e24593830b770f661fe9be49cf9ac1a
Quentin Guillou <guillou.quentin.29@gmail.com>
2015-11-10 17:41:41 +0100

    Test classes (not yet implemented)

---
+++ fr.istic.m1.aco.miniediteur.v2/tests/originator/TestDeleteRecordable.java
@@ -0,0 +1,24 @@
+package originator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDeleteRecordable {
+
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRestore() {
+		fail("Not yet implemented");
+	}
+
+}
